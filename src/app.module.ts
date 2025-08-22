da439e801b0e590b1aaef57492b9890904812f55
richardcort <144558240+richardcort@users.noreply.github.com>
2025-08-22 10:50:07 -0400

    feat: Create the Assets directory and configure the static files directory
    This commit adds a new directory for static files and configures the
    application to serve content from it.
    
    The following changes were implemented:
    - **Directory Creation:** A new directory was created to store all
    static files.
    - **Server Configuration:** The application's main configuration was
    updated to correctly serve content from the new static directory.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReportGeneratorModule } from './report-generator/report-generator.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ReportGeneratorModule],
+  imports: [
+    ReportGeneratorModule,
+    ServeStaticModule.forRoot(
+      {
+        rootPath: './src/assets/images',
+        serveRoot: '/images', 
+      }
+    ),
+  ],
   controllers: [],
   providers: [],
 })
